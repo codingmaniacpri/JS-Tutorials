ffe15ebb62bd26a5092d15b1352a876c84d3d717
Priyanshi Agarwal <91204361+codingmaniacpri@users.noreply.github.com>
2024-09-20 05:10:40 +0000

    For Loops

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,47 @@
+//for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5){
+        // console.log("5 is best number")
+    }
+    // console.log(element)
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+
+    // console.log(`Outer loop values ${i}`)
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop values ${j} and inner loop ${i}`)
+        // console.log(`${i} * ${j} = ${i*j}`)
+    }
+    
+}
+
+const myArray = ["Flashman", "Superman", "Batman"]
+// console.log(myArray.length)
+for (let i = 0; i <  myArray.length; i++) { //if we do <= then will give undefined
+    const element = myArray[i];
+    // console.log(element)
+}
+
+//break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        // console.log(`Detected 5`)
+        break
+    }
+    // console.log(`Value of i is ${index}`)
+    
+}
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        // console.log(`Detected 5`)
+        continue
+    }
+    console.log(`Value of i is ${index}`)
+    
+}
